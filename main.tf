3c24a504636f8b7a25ed2bfadcdb368b775c9c22
Orsa90 <ors294@gmail.com>
2025-08-14 15:37:03 +0300

    Add refactored terraform project

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+
+        }
+        random =  {
+            source = "hashicorp/random"
+            version = "~> 3.0"
+            
+        }
+    }
+
+    required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+    region = var.aws_region
+}
+
+resource "random_id" "bucket_suffix" {
+    byte_length = 4
+}
+
+resource "aws_s3_bucket" "or_bucket" {
+    bucket = "${var.bucket_prefix}-${random_id.bucket_suffix.hex}"
+
+    force_destroy = true
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+    type = string
+    description = "AWS region"
+    default = "us-east-1"
+}
+
+variable "bucket_prefix" {
+    type = string
+    description = "bucket name"
+    default = "or-sniper-bucket"
+
+}
